7d063d97ffd415f8de242a7ffdffc840b0d28a01
Qasim Abbas <qasimabbas52@gmail.com>
2016-10-01 02:55:46 +0000

    Setting up the environment

---
+++ mymalloc.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+static char myblock[5000];
+
+void* mymalloc(int size){
+
+}
+
+
+int main(){
+    printf("Hello World\n");
+    return 0;
+}
---
+++ mymalloc.h
@@ -0,0 +1,7 @@
+//node struct
+typedef struct Node{
+    int size;
+    int addr;
+    Node* nxt;
+}Node;
+
